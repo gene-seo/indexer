c382b38c84f709a1306d20c3532a38701a120f12
gene-seo <117685146+gene-seo@users.noreply.github.com>
2024-08-22 10:58:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 
 # Constants
 SCOPES = ['https://www.googleapis.com/auth/indexing', 'openid', 'https://www.googleapis.com/auth/userinfo.email']
-REDIRECT_URI = 'https://your-app-name.streamlit.app'  # Update this with your Streamlit Cloud URL
+REDIRECT_URI = 'https://indexer-udtjm78jffotdonak9obsp.streamlit.app'  # Update this with your Streamlit Cloud URL
 
 def create_flow():
     client_config = json.loads(st.secrets["google_oauth"]["client_secret"])
